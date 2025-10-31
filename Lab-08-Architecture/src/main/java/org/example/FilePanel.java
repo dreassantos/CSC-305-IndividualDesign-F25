35bcbd208a92c5c85f8e11f51ba1c074a13a0d66
Andrea Salazar Santos <45272203+dreassantos@users.noreply.github.com>
2025-10-31 16:18:30 -0700

    Add files via upload

---
+++ Lab-08-Architecture/src/main/java/org/example/FilePanel.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FilePanel extends JPanel {
+    FilePanel() {
+        JPanel filePanel = new JPanel();
+        filePanel.setLayout(new GridLayout(11, 1));
+    }
+}
